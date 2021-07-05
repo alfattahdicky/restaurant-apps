a54d9a580173ee84782597fd3046c728048b1da4
alfattahdicky <alfattahdicky2@gmail.com>
2021-07-05 16:38:52 +0700

    fix styling for mobile approach

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://dicoding-restaurant-api.el.r.appspot.com/',
   BASE_URL_IMAGE: 'https://dicoding-restaurant-api.el.r.appspot.com/images/medium/',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'Hungry-Apps-V1',
   DATABASE_NAME: 'hungry-apps-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'hungrys',
